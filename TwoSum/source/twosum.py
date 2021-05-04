0f0f11936cb0b45476d7eb04e896232c73b8e1e3
Victor-Tilve <ing.victortilve@gmail.com>
2021-05-04 00:02:14 -0500

    Leetcodo - TwoSum Solution

--- TwoSum/source/twosum.py
+++ TwoSum/source/twosum.py
@@ -1,25 +1,44 @@
-'''
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
+nums = [3,5,4,3,5,8,7,4]
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-'''
 
-class Solution():
-    def __init__(self):
-        self.hash = {}
+def twosum(nums,target):
+    h_nums = [hash(value) for value in nums]
+    output = {}
+    position = 0
+    solutions = []
+    evaluated = []
 
-    def twosum(self, nums, target):
-        '''
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        '''
+    #creación del diccionario con todos los keys(hash values) y valores
+    for value,_hash in zip(nums,h_nums):
+        if _hash in output:
+            #la tabla hash tiene por cada key un par de valores que corresponden la posición y el valor de un dato especifico en la lista de posibles valores
+            output[_hash].append((value, position))
+            position += 1
+            continue
+        output[_hash] = [(value,position)]
+        position += 1
+    
+    #Valido las posibles respuestas que se puedan encontrar dado un target y una lista de numero.
+    for key in output:
+        _hash = hash(target - output[key][0][0])
         
-        self.hash = {k:v for k,v in zip(hash(nums),nums)}
+        if _hash > 0 and _hash in output:
+            #Valido la cantidad de valores que se encuentran dentro de un mismo key
+            for i in range(len(output[key])):
+                for j in range(len(output[_hash])):
+                    if key not in evaluated and _hash not in evaluated and output[_hash][j][1] != output[key][i][1]:
+                        solutions.append((output[key][i][1],output[_hash][j][1]))
+                if _hash not in evaluated:
+                    evaluated.append(_hash)
+            if key not in evaluated:
+                evaluated.append(key)
+                    
+    if len(solutions) > 0:
+        print(solutions)
+    else:
+        print("No two sum solution")
 
-        for i in len(self.hash):
-            
+
+if __name__ == "__main__":
+
+    twosum(nums,8)
